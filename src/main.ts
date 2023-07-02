2c89b136e697c218da78129657e918321903bfea
Elsen01 <elsen@gmail.com>
2023-07-02 21:22:28 +0400

    User module and Registration

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { PrismaService } from './prisma.service'
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule)
+	const prismaService = app.get(PrismaService)
+	await prismaService.enableShutdownHooks(app)
+
+	const config = new DocumentBuilder()
+		.setTitle('Rossmann-Shop')
+		.setDescription('The shop API description')
+		.setVersion('1.0')
+		.addTag('Online-Shop')
+		.build()
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('api', app, document)
+	app.enableCors()
+	await app.listen(3005)
+}
+
+bootstrap()
