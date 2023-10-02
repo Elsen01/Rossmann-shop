08d63ac3149a6eda7f659cb500cd07601452101a
Elsen01 <elsen@gmail.com>
2023-10-02 13:57:10 +0400

    fix-code

---
+++ src/reviews/review.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from "@nestjs/common";
+import { ReviewService } from "./review.service";
+
+@Controller("reviews")
+export class ReviewController {
+  constructor(private readonly reviewService: ReviewService) {
+  }
+}
---
+++ src/statistics/statistic.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from "@nestjs/common";
+import { StatisticService } from "./statistic.service";
+
+@Controller("statistics")
+export class StatisticController {
+  constructor(private readonly statisticService: StatisticService) {
+  }
+}
