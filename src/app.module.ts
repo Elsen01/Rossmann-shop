b3af55e03c76d445587fa280ece7bf428558ebaa
Elsen01 <elsen@gmail.com>
2023-10-11 04:17:18 +0400

    create-product-endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,19 @@
-import { Module } from '@nestjs/common'
-import { AuthModule } from './auth/auth.module'
-import { PrismaService } from './prisma.service'
-import { ConfigModule } from '@nestjs/config'
-import { UserModule } from './users/user.module'
-import { CategoryModule } from './categories/category.module'
+import { Module } from "@nestjs/common";
+import { AuthModule } from "./auth/auth.module";
+import { PrismaService } from "./prisma.service";
+import { ConfigModule } from "@nestjs/config";
+import { UserModule } from "./users/user.module";
+import { CategoryModule } from "./categories/category.module";
 import { ReviewModule } from "./reviews/review.module";
 import { StatisticModule } from "./statistics/statistic.module";
 import { OrderModule } from "./orders/order.module";
+import { ProductModule } from "./products/product.module";
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule, CategoryModule,ReviewModule,StatisticModule,OrderModule],
-	controllers: [],
-	providers: [PrismaService]
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule, CategoryModule, 
+    ReviewModule, StatisticModule, OrderModule,ProductModule],
+  controllers: [],
+  providers: [PrismaService]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { PaginationService } from "../pagination/pagination.service";
 import { EnumProductSort, GetAllProductDto } from "./dto/get-all.product.dto";
 import { Prisma } from "@prisma/client";
 import { PrismaService } from "../prisma.service";
+import { productReturnObject, productReturnObjectFullest } from "./return-product.object";
+import { ProductDto } from "./dto/product.dto";
+import { generateSlug } from "../utils/generate-slug";
 
 @Injectable()
 export class ProductService {
@@ -27,34 +30,33 @@ export class ProductService {
     }
 
     const prismaSearchTermFilter: Prisma.ProductWhereInput = searchTerm ? {
-      OR: [
-        {
+        OR: [
+          {
 
-          category: {
+            category: {
+              name: {
+                contains: "searchTerm",
+                mode: "insensitive"
+              }
+            }
+          },
+          {
             name: {
               contains: "searchTerm",
               mode: "insensitive"
             }
-          }
-        },
-        {
-          name: {
-            contains: "searchTerm",
-            mode: "insensitive"
-          }
-        },
-        {
-          description: {
-            contains: "searchTerm",
-            mode: "insensitive"
-          
-        }
-    }
-  ]
+          },
+          {
+            description: {
+              contains: "searchTerm",
+              mode: "insensitive"
 
-  } :
-    {
-    }
+            }
+          }
+        ]
+
+      } :
+      {};
 
     // @ts-ignore
     const { perPage, skip } = this.paginationService.getPagination(dto);
@@ -73,4 +75,100 @@ export class ProductService {
     };
 
   }
+
+  async byId(id: number) {
+    const product = await this.prisma.product.findUnique({
+      where: {
+        id
+      }, select: productReturnObjectFullest
+    });
+    if (!product) throw new NotFoundException("Product Not Found");
+    return product;
+
+  }
+
+  async bySlug(slug: string) {
+    const product = await this.prisma.product.findUnique({
+      where: {
+        slug
+      },
+      select: productReturnObjectFullest
+    });
+    if (!product) throw new NotFoundException("Product Not Found");
+    return product;
+  }
+
+  async byCategory(categorySlug: string) {
+    const products = await this.prisma.product.findMany({
+      where: {
+        category: {
+          slug: categorySlug
+        }
+      },
+      select: productReturnObjectFullest
+    });
+    if (!products) throw new NotFoundException("Product Not Found");
+    return products;
+  }
+
+  async getSimilar(id: number) {
+    const currentProduct = await this.byId(id);
+
+    if (!currentProduct) throw new NotFoundException("Current Product Not Found");
+
+
+    const products = await this.prisma.product.findMany({
+      where: {
+        category: {
+          name: currentProduct.name
+        }, NOT: {
+          id: currentProduct.id
+        }
+      },
+      orderBy: {
+        createdAt: "desc"
+      },
+      select: productReturnObject
+    });
+    return products;
+  }
+
+  async create() {
+    const product = await this.prisma.product.create({
+      data: {
+        description: "",
+        slug: "",
+        name: "",
+        price: 0
+      }
+    });
+    return product.id;
+  }
+
+  async update(id: number, dto: ProductDto) {
+    const { description, name, price, images, categoryId } = dto;
+
+    return this.prisma.product.update({
+      where: {  
+        id
+      },
+      data: {
+        description,
+        name,
+        images,
+        price,
+        slug: generateSlug(name),
+        category: {
+          connect: {
+            id: categoryId
+          }
+        }
+      }
+    });
+  }
+  
+  async delete(id: number) {
+    return this.prisma.product.delete({where: { id }})
+  }
+
 }
